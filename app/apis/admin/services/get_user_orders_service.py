d9020009b10b1c1644409c4438c37d0cae83ef04
alexdavid <joelarga2020@gmail.com>
2025-11-14 19:23:30 -0500

    mitigando vulneravilidad

--- app/apis/admin/services/get_user_orders_service.py
+++ app/apis/admin/services/get_user_orders_service.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from db.session import get_db
 from sqlalchemy.orm import Session
 from apis.auth.utils import get_current_user
 from typing_extensions import Annotated
+from db.models import UserRole
 from db.models import User, Order
 
 router = APIRouter()
@@ -13,13 +14,17 @@ def get_user_orders(
     current_user: Annotated[User, Depends(get_current_user)],
     db: Session = Depends(get_db),
 ):
-    # VULNERABILIDAD INTENCIONAL:
-    # No valida que el usuario tenga rol de ADMIN
-    # No valida que user_id corresponda al usuario autenticado
+
+    # Validación de control de acceso basada en roles (RBAC) y propiedad del recurso
+    if current_user.role != UserRole.ADMIN.value and current_user.id != user_id:
+        raise HTTPException(
+            status_code=403,
+            detail="Unauthorized access: You cannot view other users' orders"
+        )
 
     orders = db.query(Order).filter(Order.user_id == user_id).all()
     return {
-        "message": "Confidential orders retrieved successfully",
+        "message": "Orders retrieved successfully",
         "orders": orders,
         "requested_for_user_id": user_id,
         "current_user_role": current_user.role,
